6ece8b7188ca7644195ed6bec4de9cc500169b63
coyotte508 <coyotte508@gmail.com>
2023-01-13 13:55:11 +0100

    🔥 Remove source

--- packages/website/src/theme/MDXComponents/index.tsx
+++
@@ -1,9 +0,0 @@
-import MDXComponents from '@theme-original/MDXComponents';
-
-import { RuleAttributes } from './RuleAttributes';
-
-// eslint-disable-next-line import/no-default-export
-export default {
-  ...MDXComponents,
-  'rule-attributes': RuleAttributes,
-};
